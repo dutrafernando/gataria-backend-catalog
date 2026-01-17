d3e2049d320724692e245f258bfe611edbc0f907
f3rnandojr <fjr.dutra@gmail.com>
2026-01-17 10:32:38 -0500

    newbase ci/cd

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,2 +1,2 @@
-export const mongoURL = "mongodb+srv://admingataria:admingataria@gataria-database.ehc4v.mongodb.net/";
+export const mongoURL = "mongodb+srv://admin:admin@mongodb.k8ovxdo.mongodb.net/?appName=mongodb";
 export const mongoDatabase = "dev";
